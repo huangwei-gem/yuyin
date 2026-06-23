6fbc05407bba257cc8854e491ca0b7fd6e3ec1d3
huangwei-gem <huangwei.gem@gmail.com>
2026-06-23 17:27:54 +0800

    chore: cleanup temp files

--- fix.mjs
+++
@@ -1,5 +0,0 @@
-﻿import fs from "fs";
-let code = fs.readFileSync("src/index.ts", "utf8");
-code = code.replace('INDEX_HTML_B64 = ""', "INDEX_HTML_B64 = \"");
-fs.writeFileSync("src/index.ts", code);
-console.log("Fixed");
